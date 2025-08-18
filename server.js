4e4eb32f8391d977d5c4741a14c136f030803b30
ArrMannDev <arrmann632025@gmail.com>
2025-08-18 09:07:56 +0630

    Install packages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
@@ -8,6 +9,9 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set("views",path.join(__dirname,"views"));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
