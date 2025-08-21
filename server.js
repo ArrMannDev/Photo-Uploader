8f20f5b44ab500834e448c47ce21723983ec1c25
ArrMannDev <arrmann632025@gmail.com>
2025-08-21 20:59:07 +0630

    Set Up Finished

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.set('view engine', 'ejs');
 app.set("views",path.join(__dirname,"views"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render('index');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
 });
