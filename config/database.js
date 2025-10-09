19c29dc58d1f4dd4a2a74cdacdb6328bbc71f73d
ArrMannDev <arrmann632025@gmail.com>
2025-10-09 18:04:25 +0630

    intial project

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'photo_uploader',
-    password:""
+const connection = mysql.createConnection({
+  host: "sql12.freesqldatabase.com",
+  user: "sql12802081",
+  password: "cYKE4kmFuv",
+  database: "sql12802081",
+  port: 3306
 });
 
-module.exports = pool.promise();
+module.exports = connection;
+
