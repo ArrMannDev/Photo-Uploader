a8185671074570e7a2ef917ecfcf1acf338c6e87
ArrMannDev <arrmann632025@gmail.com>
2025-10-09 21:08:26 +0630

    final touch

--- config/database.js
+++ config/database.js
@@ -1,12 +1,21 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12802081",
-  password: "cYKE4kmFuv",
-  database: "sql12802081",
+// const connection = await mysql.createConnection({
+//   host: "sql12.freesqldatabase.com",
+//   user: "sql12802081",
+//   password: "cYKE4kmFuv",
+//   database: "sql12802081",
+//   port: 3306
+// });
+
+// module.exports = connection;
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "photo_uploader",
   port: 3306
 });
 
-module.exports = connection;
-
+module.exports = pool;
